1c6745e162a80cbcc1782e72b7de9a653be90722
Ladislav Martincik <ladislav.martincik@gmail.com>
2009-03-16 13:42:49 +0100

    - Adding changes by Joseph Jaramillo

--- lib/mem_cache_store_with_delete_matched.rb
+++ lib/mem_cache_store_with_delete_matched.rb
@@ -1,22 +1,22 @@
 class MemCacheStoreWithDeleteMatched < ActiveSupport::Cache::MemCacheStore
-
+ 
   module Common
     KEY = 'memcached_store_key_list'
   end
   
   def write(name, value, options = nil)
-    key_list = YAML.load(read(Common::KEY)) rescue []
+    key_list = get_key_list
     unless key_list.include?(name)
-      key_list << name 
+      key_list << name
       return false unless _write(Common::KEY, key_list.to_yaml, options)
     end
     
     _write(name, value, options)
   end
-
+ 
   def delete_matched(matcher, options = nil)
     keys_to_remove = []
-    key_list = YAML.load(read(Common::KEY))
+    key_list = get_key_list
     key_list.each do |name|
       if name.match(matcher)
         return false unless delete(name, options)
@@ -29,15 +29,23 @@ class MemCacheStoreWithDeleteMatched < ActiveSupport::Cache::MemCacheStore
     _write(Common::KEY, key_list.to_yaml, options)
   end
   
-  private 
-  
-    def _write(key, value, options = nil)
-      method = options && options[:unless_exist] ? :add : :set
-      response = @data.send(method, key, value, expires_in(options), raw?(options))
-      return true if response.nil?
-      response == Response::STORED
-    rescue MemCache::MemCacheError => e
-      logger.error("MemCacheError (#{e}): #{e.message}")
-      false
+  private
+ 
+  def get_key_list
+    begin
+      YAML.load(read(Common::KEY))
+    rescue TypeError
+      []
     end
+  end
+  
+  def _write(key, value, options = nil)
+    method = options && options[:unless_exist] ? :add : :set
+    response = @data.send(method, key, value, expires_in(options), raw?(options))
+    return true if response.nil?
+    response == Response::STORED
+  rescue MemCache::MemCacheError => e
+    logger.error("MemCacheError (#{e}): #{e.message}")
+    false
+  end
 end
